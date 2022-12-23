1edbef3bad228cda47bab923f2926b1255d09350
Shuvam <ssiwakoti12@gmail.com>
2022-12-23 10:09:53 +0545

    deployment changes for vercel

--- server.js
+++ server.js
@@ -5,22 +5,29 @@ dotenv.config();
 
 const port = process.env.PORT;
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+export default app;
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// const server = app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
 
-process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
-});
+// process.on('unhandledRejection', (err) => {
+//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+//   console.log(err.name, err.message);
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+// process.on('SIGTERM', () => {
+//   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+//   server.close(() => {
+//     console.log('💥 Process terminated!');
+//   });
+// });
+
